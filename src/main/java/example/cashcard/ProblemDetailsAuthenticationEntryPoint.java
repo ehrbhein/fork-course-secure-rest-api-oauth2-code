f3d1aca5b045a21b758fab243cc8bf8d9deeac46
ehrbhein <theofficial.irvingil@gmail.com>
2024-10-02 21:07:10 +0800

    feat: add security-filter-chain

--- src/main/java/example/cashcard/ProblemDetailsAuthenticationEntryPoint.java
+++ src/main/java/example/cashcard/ProblemDetailsAuthenticationEntryPoint.java
@@ -21,5 +21,6 @@ public class ProblemDetailsAuthenticationEntryPoint implements AuthenticationEnt
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
 			throws IOException, ServletException {
+		response.setStatus(401);
 	}
 }
